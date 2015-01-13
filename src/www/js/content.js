9455b15af701b85d0a5e14c3ec359ec4508b5343
Joe <niemijoe@hotmail.com>
2015-01-13 13:16:41 +0200

    timer working

--- src/www/js/content.js
+++ src/www/js/content.js
@@ -39,10 +39,10 @@ var Settings = React.createClass({
 var timePlayer1; // 10 of this is one second
 var timePlayer2; // 10 of this is one second
 var paused = false;
-var turn = player1;
+var turn = "";
 //var incrementP1 = 0;
 //var incrementP2 = 0;
-//var startNew = false;
+var startNew = true;
 
 
 var Timer = React.createClass({
@@ -50,10 +50,7 @@ var Timer = React.createClass({
     getInitialState: function() {
       
         return {
-            player1: 0,
-            player2: 0,
-            turn: "player1",
-            paused: true
+
         };
     },
 
@@ -61,59 +58,127 @@ var Timer = React.createClass({
         var timeMin = document.getElementById("timer-setting").value * 60 * 10;
         timePlayer1 = timeMin;
         timePlayer2 = timeMin;
-        console.log(this.state.player1);
-        console.log("vuoro " + this.state.turn);
+        this.setState({
+            startTime: timeMin
+        });
+        this.setTimerToPlayer("player1");
+        this.setTimerToPlayer("player2");
+        this.timer = setInterval(this.tick, 100);
     },
 
     changeTurn: function(player) {
-        
-        console.log("turn on " + this.state.turn);
-        if (this.state.paused) {
-            console.log("LOL");
+        if (startNew) {
+            this.changePlayerTurn(player);
             return;
         }
-        console.log("PELAAJA ON " + player);
-        if (player == player1) {
-            console.log("WAT");   
-            if (this.state.turn == "player1") {
-                this.setState({turn: "player2"});
-           //     timePlayer1 = (parseInt(timePlayer1) + parseInt(incrementP1));
-            }
-                      
-        } else {
-            if (this.state.turn == "player2") {
-                console.log("WUT");
-                this.setState({turn: "player1"});
-             //   timePlayer2 = (parseInt(timePlayer2) + parseInt(incrementP2));
-            
-            }
+        if (paused) {
+            console.log("game was paused");
+            return;
         }
+        this.changePlayerTurn(player);
         
-        console.log("turn on " + this.state.turn);
+        console.log("turn changed to " + turn);
         
     },
 
-    settings: function() {
+    changePlayerTurn: function(player) {
+        if(startNew) {
+             if (player == "player1") {
+                 turn = "player2";
+             } else {
+                 turn = "player1";
+             }
+             startNew = false;
+        } else {
+            if (player == "player1") {
+                if (turn == "player1") {
+                    turn = "player2";
+               //     timePlayer1 = (parseInt(timePlayer1) + parseInt(incrementP1));
+                }        
+            } else {
+                if (turn == "player2") {
+                    turn = "player1";
+                 //   timePlayer2 = (parseInt(timePlayer2) + parseInt(incrementP2));
+                }
+            }
+        }
+    },
 
+    setTimerToPlayer: function(player) {
+        
+        var playerTime;
+        if (player == "player1") {
+            playerTime = timePlayer1; 
+        } else {
+            playerTime = timePlayer2;
+        }
+
+        var mins = Math.floor(playerTime/10/60);
+        var secs = (Math.floor(playerTime/10) - mins*60);
+        if (secs <= 9) {
+            secs = "0" + secs
+        }
+        var milSecs = playerTime.toString().charAt(playerTime.toString().length-1);
+        
+        document.getElementById(player).innerHTML = mins + ":" + secs + ":" + milSecs;
+    
+    },
+
+    tick: function() {
+        if (paused) {
+            return;
+        }
+        if (startNew == true) {
+            return;
+        }
+        if (turn == "player1") {
+            timePlayer1--;
+        } else {
+            timePlayer2--;
+        }
+        this.setTimerToButtons();
+        var gameEnd = this.isGameEnded();
+        if (gameEnd == "gameContinues") {
+           // setTimeout('Decrease()', 100);
+        } else {
+            this.endGame(gameEnd);
+        }
+    },
+
+    setTimerToButtons: function() {
+        this.setTimerToPlayer("player1");
+        this.setTimerToPlayer("player2");
+    },
+
+    isGameEnded: function() {
+        if (timePlayer1 <= 0) {
+            return "player2wins"
+        } else if (timePlayer2 <= 0) {
+            return "player1wins"
+        }
+        return "gameContinues";
     },
 
     pauseGame: function() {
-    console.log("game pause changed");
-    if (paused) {
-        paused = false;
-        this.setState({paused: false });
-    //    changePauseText("Pause");
-     //   setTimeout('Decrease()',100);
-    } else {
-        paused = true;
-        this.setState({paused: true });
-   //     changePauseText("Unpause");
-    }
-    console.log("paused " + paused);
+        console.log("game pause changed");
+        if (paused) {
+            paused = false;
+        //    changePauseText("Pause");
+         //   setTimeout('Decrease()',100);
+        } else {
+            paused = true;
+       //     changePauseText("Unpause");
+        }
+        console.log("paused " + paused);
     },
 
     newGame: function() {
-
+        newGame = true;
+        paused = true;
+        timePlayer1 = this.state.startTime;
+        timePlayer2 = this.state.startTime;
+        this.setTimerToPlayer("player1");
+        this.setTimerToPlayer("player2");
     },
 
     render: function () {
@@ -125,7 +190,7 @@ var Timer = React.createClass({
             </div>
             <div className="row">
                    <button className="menuButton" id="pauseButton" onClick={this.pauseGame.bind(this, "")}>Pause</button>
-                   <button className="menuButton" id="newGameButton" onClick={this.newGame()}>New Game</button>
+                   <button className="menuButton" id="newGameButton" onClick={this.newGame.bind(this, "")}>New Game</button>
             </div>
             <div className="row">
                <button className="playButton" id="player2" onClick={this.changeTurn.bind(this, "player2")}>4234</button>
